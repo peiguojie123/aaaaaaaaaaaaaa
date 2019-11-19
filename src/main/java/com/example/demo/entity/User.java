12a1eb6eb1f6dc4a573ad061bcff85acb9eb776b
peiguojie <1013956626@qq.com>
2019-11-19 16:50:44 +0800

    首次提交，新建springboot工程

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+public class User {
+    private String id;
+
+
+}
